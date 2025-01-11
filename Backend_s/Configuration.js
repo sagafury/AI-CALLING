460e70496a6d2b47278f574036946439cae11ca0
sagafury <xashishrautela190@gmail.com>
2025-01-11 08:57:37 +0530

    Delete Backend_s directory

--- Backend_s/Configuration.js
+++
@@ -1,9 +0,0 @@
-class Configuration {
-    constructor({ apiKey, basePath = "https://api.openai.com/v1", baseOptions = {} }) {
-        this.apiKey = apiKey;
-        this.basePath = basePath;
-        this.baseOptions = baseOptions;
-    }
-}
-
-module.exports = Configuration;
